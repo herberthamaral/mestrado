c86b443c0111a550635c656d9714b205b5bec9bc
Herberth Amaral <herberthamaral@gmail.com>
2015-09-28 15:16:16 -0300

    Melhora alguns operadores

--- CE/caixapreta.py
+++ CE/caixapreta.py
@@ -2,11 +2,10 @@
 """
 Primeiro exercício de computação evolutiva.
 """
-
 import sys
-from random import randint, random, uniform, seed
-
-seed()
+import numpy as np
+import matplotlib.pyplot as plt
+from random import randint, random, uniform, seed, shuffle
 
 def individuo():
     return [randint(0,1) for x in range(36)]
@@ -14,41 +13,44 @@ def individuo():
 def populacao(tamanho):
     return [individuo() for x in range(tamanho)]
 
-def fitness(individuo, objetivo):
+def fitness(individuo):
     i = individuo
-    f = 9+i[1]*i[4]-i[22]*i[13]+i[23]*i[3]-i[20]*i[9]+i[35]*i[14]-i[10]*i[25]+i[15]*i[16]+i[2]*i[32]+i[27]*i[18]+i[11]*i[33]-i[30]*i[31]-i[21]*i[24]+i[34]*i[26]-i[28]*i[6]+i[7]*i[12]-i[5]*i[8]+i[17]*i[19]-i[0]*i[29]+i[22]*i[3]+i[20]*i[14]+i[25]*i[15]+i[30]*i[11]+i[24]*i[18]+i[6]*i[7]+i[8]*i[17]+i[0]*i[32]
+    f = 9+i[1]*i[4]-i[22]*i[13]+i[23]*i[3]-i[20]*i[9]+i[35]*i[14]-i[10]*i[25]+i[15]*i[16]+i[2]*i[32]\
+            +i[27]*i[18]+i[11]*i[33]-i[30]*i[31]-i[21]*i[24]+i[34]*i[26]-i[28]*i[6]+i[7]*i[12]-i[5]\
+            *i[8]+i[17]*i[19]-i[0]*i[29]+i[22]*i[3]+i[20]*i[14]+i[25]*i[15]+i[30]*i[11]+i[24]*i[18]\
+            +i[6]*i[7]+i[8]*i[17]+i[0]*i[32]
     return f
 
-def fitness_medio_populacao(populacao, objetivo):
-    soma = sum([fitness(individuo, objetivo) for individuo in populacao])
-    return soma/len(populacao)
+media = lambda x: sum(x)/float(len(x))
 
-def ordena_melhores(populacao, objetivo):
-    melhores = [(objetivo - fitness(individuo, objetivo), individuo) for individuo in populacao]
-    melhores = [x[1] for x in sorted(melhores)]
+def fitness_medio_populacao(populacao):
+    return media([fitness(individuo) for individuo in populacao])
+
+def ordena_melhores(populacao):
+    melhores = [(fitness(individuo), individuo) for individuo in populacao]
+    melhores = [x[1] for x in sorted(melhores)[::-1]]
     return melhores
 
-def selecao_normal(populacao, objetivo, retencao):
-    melhores = ordena_melhores(populacao, objetivo)
+def selecao_elitista(populacao, objetivo, retencao):
+    melhores = ordena_melhores(populacao)
     qtd_retencao = int(len(melhores)*retencao)
     pais = melhores[:qtd_retencao]
     return pais
 
 def selecao_roleta(populacao, objetivo, retencao):
-    fitness_populacao = sum([fitness(i, objetivo) for i in populacao])
-    ordenado = ordena_melhores(populacao, objetivo)
+    fitness_populacao = float(sum([fitness(i) for i in populacao]))
+    fmp = fitness_medio_populacao(populacao)
+    ordenado = ordena_melhores(populacao)
     def seleciona():
-        p = uniform(0, fitness_populacao)
+        p = 0
         for f in ordenado[::-1]:
-            if p <= 0:
+            p += fitness(f)/(fitness_populacao*fmp)
+            if random() <= p:
                 break
-            else:
-                p -= fitness(f, objetivo)
         return f
     selecionados = []
-    for x in range(int(len(populacao)*retencao)):
+    for x in range(int(len(populacao)*(1-retencao))):
         selecionado = seleciona()
-        ordenado.remove(selecionado)
         selecionados.append(selecionado)
     return selecionados
 
@@ -59,8 +61,8 @@ def selecao_torneio(populacao, objetivo, retencao):
     for i in range(num_individuos/2):
         i1, i2 = populacao[randint(0,t)], populacao[randint(0,t)]
         i3, i4 = populacao[randint(0,t)], populacao[randint(0,t)]
-        selecionados.append(i1 if fitness(i1, objetivo) > fitness(i2, objetivo) else i2)
-        selecionados.append(i3 if fitness(i3, objetivo) > fitness(i4, objetivo) else i4)
+        selecionados.append(i1 if fitness(i1) > fitness(i2) else i2)
+        selecionados.append(i3 if fitness(i3) > fitness(i4) else i4)
     return selecionados
 
 def mutacao_normal(individuos, taxa_mutacao):
@@ -78,7 +80,7 @@ def mutacao_uniforme(individuos, taxa_mutacao):
     return individuos
 
 
-def cruzamento_normal(pais, populacao):
+def cruzamento_ponto_corte_aleatorio(pais, populacao, probabilidade):
     qtd_pais = len(pais)
     tamanho_desejado = len(populacao) - qtd_pais
     filhos = []
@@ -88,37 +90,99 @@ def cruzamento_normal(pais, populacao):
         if macho != femea:
             macho = pais[macho]
             femea = pais[femea]
-            ponto_corte = randint(0, 35)
-            filho = macho[:ponto_corte] + femea[ponto_corte:]
+            pc = randint(0, 35)
+            filho = macho[:pc] + femea[pc:]
             filhos.append(filho)
     return filhos
 
-def evolucao(populacao, objetivo, retencao=0.2, taxa_mutacao=0.01, selecao=selecao_normal, mutacao=mutacao_uniforme, cruzamento=cruzamento_normal):
+def cruzamento_uniforme(pais, populacao, probabilidade):
+    qtd_pais = len(pais)
+    tamanho_desejado = len(populacao) - qtd_pais
+    filhos = []
+    while len(filhos) < tamanho_desejado:
+        macho = randint(0, qtd_pais-1)
+        femea = randint(0, qtd_pais-1)
+        macho = pais[macho]
+        femea = pais[femea]
+        if macho != femea:
+            filho = []
+            for i in range(36):
+                if random() > 0.5:
+                    filho.append(macho[i])
+                else:
+                    filho.append(femea[i])
+            filhos.append(filho)
+    return filhos
+
+def evolucao(populacao, fitness, objetivo, retencao=0.2, taxa_mutacao=0.01, selecao=selecao_elitista,
+        mutacao=mutacao_uniforme, cruzamento=cruzamento_ponto_corte_aleatorio, probabilidade_cruzamento=0.8):
     pais = selecao(populacao, objetivo, retencao)
-    filhos = cruzamento(pais, populacao)
+    filhos = cruzamento(pais, populacao, probabilidade_cruzamento)
     filhos = mutacao(filhos, taxa_mutacao)
     pais.extend(filhos)
     return pais
 
+def melhor_fitness(pop, objetivo=27):
+    avaliacao = [(objetivo - fitness(i), i) for i in pop]
+    melhor_fitness = fitness(avaliacao[0][1])
+    return melhor_fitness
+
+def menor_fitness(pop, objetivo=27):
+    avaliacao = [(objetivo - fitness(i), i) for i in pop]
+    melhor_fitness = fitness(avaliacao[0][1])
+    return melhor_fitness
+
+
+def teste_base(func_evolucao):
+    dados_execucao = dict(numero_sucessos=0, maior_fitness=0, menor_fitness=0, fitness_medio=0, desvio_padrao=[])
+    max_geracoes = 50
+    execucoes = 100
+    for i in range(execucoes):
+        evolucao_melhor_fitness = []
+        evolucao_fitness_medio = []
+        perda_variedade_genetica = []
+        p = populacao(30)
+        geracoes = 0
+        while geracoes<max_geracoes:
+            incremento_taxa_mutacao = len(set(evolucao_melhor_fitness))*0.0
+            p = func_evolucao(p, incremento_taxa_mutacao)
+            geracoes += 1
+            dados_execucao['desvio_padrao'].extend([fitness(j) for j in p])
+            if dados_execucao['fitness_medio'] == 0:
+                dados_execucao['fitness_medio'] = fitness_medio_populacao(p)
+                dados_execucao['menor_fitness'] = melhor_fitness(p)
+            else:
+                dados_execucao['fitness_medio'] = media([dados_execucao['fitness_medio'], fitness_medio_populacao(p)])
+                dados_execucao['menor_fitness'] = min([dados_execucao['menor_fitness'], menor_fitness(p)])
+            evolucao_melhor_fitness.append(melhor_fitness(p))
+            evolucao_fitness_medio.append(fitness_medio_populacao(p))
+            perda_variedade_genetica.append(len(set([tuple(j) for j in p])))
+
+        #print melhor_fitness(p)
+        #plt.plot(range(len(evolucao_melhor_fitness)), evolucao_melhor_fitness, 'g')
+        #plt.plot(range(len(evolucao_fitness_medio)), evolucao_fitness_medio, 'b')
+        #plt.plot(range(len(perda_variedade_genetica)), perda_variedade_genetica, 'r')
+        #plt.title(u'Evolução do melhor fitness e do fitness medio da população')
+        ##import pdb;pdb.set_trace()
+        #plt.show()
+
+        dados_execucao['numero_sucessos'] += int(melhor_fitness(p) == 27)
+        dados_execucao['maior_fitness'] = max(dados_execucao['maior_fitness'], melhor_fitness(p))
+    dados_execucao['desvio_padrao'] = np.std(dados_execucao['desvio_padrao'])
+    dados_execucao['execucoes'] = i+1
+    return dados_execucao
+
+def primeiro_teste():
+    func_evolucao = lambda populacao, incremento_taxa_mutacao: evolucao(populacao, fitness, 27, selecao=selecao_roleta,
+            retencao=0.8, taxa_mutacao=0.025+incremento_taxa_mutacao, cruzamento=cruzamento_ponto_corte_aleatorio)
+    dados_execucao = teste_base(func_evolucao)
+    print('\n'.join([chave+': '+str(dados_execucao[chave]) for chave in dados_execucao.keys()]))
+    return dados_execucao
+
 if __name__ == '__main__':
-    funcoes_selecao = {'selecao_roleta': selecao_roleta, 'selecao_torneio': selecao_torneio}
+    funcoes_teste = dict(primeiro_teste=primeiro_teste)
     if len(sys.argv) != 2:
-        print("Uso: caixapreta.py funcao_selecao")
-        print("As funções de seleção podem ser: {}".format(','.join(funcoes_selecao.keys())))
+        print("Uso: caixapreta.py [teste]")
+        print("Em que teste pode ser uma dentre: {}".format(', '.join(funcoes_teste.keys())))
         sys.exit(1)
-    selecao = funcoes_selecao[sys.argv[1]]
-    objetivo = 27
-    execucoes = []
-    for v in range(30):
-        pop = populacao(80)
-        iteracoes = 0
-        while True:
-            iteracoes += 1
-            pop = evolucao(pop, objetivo, taxa_mutacao=0.025, selecao=selecao)
-            avaliacao = [(objetivo - fitness(i, objetivo), i) for i in pop]
-            melhor_fitness = fitness(avaliacao[0][1], objetivo)
-            if melhor_fitness == objetivo:
-                print "Encontrado melhor individuo depois de {} geracoes: ".format(iteracoes), avaliacao[0][1]
-                execucoes.append(iteracoes)
-                break
-    print("Quantidade média de gerações: {}".format(sum(execucoes)/float(len(execucoes))))
+    funcoes_teste[sys.argv[1]]()
